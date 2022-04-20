8226b19cef854e4adea0fe1dd1f6b0e18aa83948
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2022-04-20 12:22:04 +0800

    Add files via upload

---
+++ Sample/Android/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+
+namespace Sample.Droid
+{
+    [Activity(Label = "Sample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            var density = Resources.DisplayMetrics.Density;
+            App.ScreenWidth = Resources.DisplayMetrics.WidthPixels / density;
+            App.ScreenHeight = Resources.DisplayMetrics.HeightPixels / density;
+            App.androidHeightPixels = Android.Content.Res.Resources.System.DisplayMetrics.HeightPixels;
+            App.DeviceType = 1;
+
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                App.ScreenHeight = (16 * App.ScreenWidth) / 9;
+                App.adjustedHeight = App.ScreenHeight;
+                App.isPhone = true;
+            }
+
+            if (Device.Idiom == TargetIdiom.Tablet)
+            {
+                App.ScreenWidth = (9 * App.ScreenHeight) / 16;
+                App.adjustedWidth = App.ScreenWidth;
+                App.isPhone = false;
+            }
+
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
