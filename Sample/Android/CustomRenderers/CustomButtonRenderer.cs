824cc318ec53873ba09d3ea15291d9e5190d3652
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2022-04-20 12:21:00 +0800

    Add files via upload

---
+++ Sample/Android/CustomRenderers/CustomButtonRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Sample;
+using Sample.Droid;
+using Android.Content;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
+namespace Sample.Droid
+{
+    class CustomButtonRenderer : ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            CustomButton elem = Element as CustomButton;
+            Control.SetAllCaps(elem.AutoCapitalization);
+        }
+    }
+}
---
+++ Sample/Android/DependencyServices/ToastService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Sample.Droid;
+using Sample;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ToastService))]
+namespace Sample.Droid
+{
+    class ToastService : iToastService
+    {
+        public void Show(string message, bool isLong)
+        {
+            Toast.MakeText(Android.App.Application.Context, message, isLong ? ToastLength.Long : ToastLength.Short).Show();
+        }
+    }
+}
