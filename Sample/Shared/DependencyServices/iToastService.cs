4b72b5e3e13d67fe3843652a5ed48ff3b691f43a
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2022-04-20 12:11:54 +0800

    Add files via upload

---
+++ Sample/Shared/DependencyServices/iToastService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample
+{
+    public interface iToastService
+    {
+        void Show(string message, bool isLong);
+    }
+}
