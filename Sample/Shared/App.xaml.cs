1056d23e7e48d415e5b39ede04276b916a4ab666
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2022-04-20 12:13:25 +0800

    Add files via upload

---
+++ Sample/Shared/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Sample
+{
+    public partial class App : Application
+    {
+        public static float ScreenWidth { get; set; }
+        public static float ScreenHeight { get; set; }
+        public static float originalHeight { get; set; }
+        public static float adjustedHeight { get; set; }
+        public static float originalWidth { get; set; }
+        public static float adjustedWidth { get; set; }
+        public static float androidHeightPixels { get; set; }
+        public static float appScale { get; set; }
+        public static int DeviceType { get; set; }
+        public static bool isPhone { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            if (Application.Current.Properties.ContainsKey("email") && Application.Current.Properties.ContainsKey("name"))
+            {
+                Application.Current.MainPage = new SampleTabbedPage(
+                    Application.Current.Properties["name"].ToString(), Application.Current.Properties["email"].ToString()
+                );
+            }
+            else
+            {
+                MainPage = new NavigationPage(new MainPage());
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            //Console.WriteLine("App starts");
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+            //Console.WriteLine("App sleeps");
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+            //Console.WriteLine("App resumes");
+        }
+    }
+}
---
+++ Sample/Shared/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
+            NameEntry.Text = "Name";
+            EmailEntry.Text = "Email@gmail.com";
+            PasswordEntry.Text = "TabbedPage";
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(NameEntry.Text) && !string.IsNullOrEmpty(EmailEntry.Text) && !string.IsNullOrEmpty(PasswordEntry.Text))
+            {
+                Xamarin.Forms.Application.Current.Properties["name"] = NameEntry.Text;
+                Xamarin.Forms.Application.Current.Properties["email"] = EmailEntry.Text;
+                Xamarin.Forms.Application.Current.SavePropertiesAsync();
+
+                Xamarin.Forms.Application.Current.MainPage = new SampleTabbedPage(NameEntry.Text, EmailEntry.Text);
+                /*
+                //Use this if you want the master detail page
+                Application.Current.MainPage = new MainMasterDetailPage(NameEntry.Text, EmailEntry.Text); 
+                */
+            }
+            else
+            {
+                bool retryBool = await DisplayAlert("Error", "Please fill in all fields. Retry?", "Yes","No");
+                if (retryBool)
+                {
+                    NameEntry.Text = string.Empty;
+                    EmailEntry.Text = string.Empty;
+                    PasswordEntry.Text = string.Empty;
+                    NameEntry.Focus();
+                }
+            }
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            //logo.Rotation = 90;//animation without time
+            //await logo.RotateTo(90, 5000);//animation with time
+
+            //logo.Scale = 2;//animation without time
+            //await logo.ScaleTo(2, 5000);//animation with time
+
+            //logo.TranslationX = 50;//animation without time
+            //logo.TranslationY = 50;//animation without time
+            //await logo.TranslateTo(50, 50, 5000);//animation with time
+
+            //logo.Opacity = 0.5;//animation without time
+            //await logo.FadeTo(0.1, 5000);//animation with time
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            //NameEntry.Text = string.Empty;
+            //EmailEntry.Text = string.Empty;
+            //PasswordEntry.Text = string.Empty;
+        }
+
+        private async void SignUp_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new SignupPage(), true);
+            //await Navigation.PushAsync(new SignupPage(), false);
+        }
+    }
+}
